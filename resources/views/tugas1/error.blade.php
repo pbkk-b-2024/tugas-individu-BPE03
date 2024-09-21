533a479320730ceff41c6ec25e98d0ba18047955
Bimantara Putra Ernandra <bima.pe02@gmail.com>
2024-09-21 12:45:57 +0700

    reset main

--- resources/views/tugas1/error.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('dashboard.base')
-
-@section('title', '404 Error')
-
-@section('content')
-<h1>Error 404 Not Found</h1>
-
-@endsection
--- resources/views/tugas1/named-route.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('dashboard.base')
-
-@section('title', 'Named Route')
-
-@section('content')
-<h1>Konten Named Route</h1>
-@endsection
